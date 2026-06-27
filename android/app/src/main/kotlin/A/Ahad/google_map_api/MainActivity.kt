9ea793d76bbcadf0b50eb1c7bd2017d1071422d8
aakhan917720 <asamadk112452@gmail.com>
2026-06-27 11:57:03 +0500

    Google

---
+++ android/app/src/main/kotlin/A/Ahad/google_map_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package A.Ahad.google_map_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text("Google Map", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
+      //   backgroundColor: Colors.blue,
+      //   centerTitle: true,
+      // ),
+      body: GoogleMap(
+        initialCameraPosition: _kGooglePlex,
+      ),
+    );
+  }
+}
